2dc005ae7b41c3651c896ae017d04990ebe1b6e7
Ferolinder <julien.maret@isen-ouest.yncrea.fr>
2025-03-03 17:13:20 +0100

    ajout de quelque commentaires + fichier de test pour moa

--- fir.c
+++ fir.c
@@ -1,8 +1,18 @@
 #include "fir.h"
 
 absorp firTest(char* filename){
+  /*
+	Filtre toutes les données du fichier associé et renvoie la dernière valeur filtrée
+	*/
 	absorp	myAbsorp;
 	
 	return myAbsorp;
+
+}
+
+absorp fir(absorp* input){
+  /*
+	Transforme les données de la structure d'entrée (filtre)
+   */
 
 }
---
+++ test.c
@@ -0,0 +1,6 @@
+/*
+    A ajouter dans le main (fonction qui vas lire les données du fichier 'file'
+    et les envoyer dans la structure absorp)
+ */
+
+gi
